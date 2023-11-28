8595e4d3b7a81108d1579bc552b74a5d656a61b8
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-28 17:47:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+var container = document.getElementById('boxConatiner');
+for (var i=0; i<800; i++){
+	var box = document.creatElement('div');
+	box.className = 'square';
+	container.appendChild(box);
+}
