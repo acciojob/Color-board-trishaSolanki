9fb80edff3cd21057c116413b7a6b832b27830b2
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-28 17:51:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //your JS code here. If required.
-var container = document.getElementById('boxConatiner');
-for (var i=0; i<800; i++){
-	var box = document.creatElement('div');
-	box.className = 'square';
-	container.appendChild(box);
-}
+ var container = document.getElementById('boxContainer');
+
+  // Generate 800 boxes dynamically
+  for (var i = 0; i < 800; i++) {
+    var box = document.createElement('div');
+    box.className = 'square';
+
+    // Append the box to the container
+    container.appendChild(box);
+  }
