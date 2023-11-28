a1e19e8530a9a5198ed34b48960fa50ab27c6609
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-28 20:01:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 //your JS code here. If required.
- var container = document.getElementById('boxContainer');
 
-  // Generate 800 boxes dynamically
-  for (var i = 0; i < 800; i++) {
-    var box = document.createElement('div');
-    box.className = 'square';
 
-    // Append the box to the container
-    container.appendChild(box);
-  }
+//your JS code here. If required.
+const container = document.getElementById('color-board');
+
+        // Function to generate a random hex color
+        function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
+        // Create 800 squares with random background color
+        for (let i = 0; i < 800; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+
+            // Add a transition end event listener to smoothly revert the color
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = getRandomColor();
+            });
+
+			square.addEventListener('mouseout', () => {
+                square.style.backgroundColor = '#1d1d1d';
+            });
+
+            container.appendChild(square);
+        }
